127bd6d0d967e02b0c2876bcc098fbe72f5fb086
Rajeshhh09 <rajeshmali8900@gmail.com>
2026-03-13 11:53:23 +0530

    Update MenuPage.js

--- frontend/src/pages/MenuPage.js
+++ frontend/src/pages/MenuPage.js
@@ -1944,7 +1944,7 @@ export default function MenuPage() {
             {filteredMenu.length === 0 ? (
               <div className="empty-st">
                 <div style={{ fontSize:'3.5rem', marginBottom:'.75rem' }}>🍽️</div>
-                <div style={{ fontFamily:"'Playfair Display',serif", fontSize:'1.2rem', color:'var(--text3)' }}>No dishes found</div>
+                <div style={{ fontFamily:"'Playfair Display',serif", fontSize:'1.2rem', color:'var(--text3)' }}>Wait For Few Seconds Dishes are Loading</div>
               </div>
             ) : (
               <div className="mgrid">
